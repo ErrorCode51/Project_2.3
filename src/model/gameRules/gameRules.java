2108e957fe8358f314cacc425b236eea3f8f193d
ErrorCode51 <43345236+errorcode51@users.noreply.github.com>
2019-04-01 20:54:07 +0200

    added the basis for the ai

---
+++ src/model/gameRules/gameRules.java
@@ -0,0 +1,11 @@
+package model.gameRules;
+
+import model.playingField.playingField;
+
+public interface gameRules {
+
+    public int getValidMoves(playingField field); // returns a list of all the moves to which a piece may be placed
+//    TODO: een return type kiezen
+
+//    TODO: meer functies toevoegen
+}
---
+++ src/model/playingField/playingField.java
@@ -0,0 +1,10 @@
+package model.playingField;
+
+import java.util.ArrayList;
+
+// this class is a playing field for one of the board games
+// this class contains an arraylist of tiles
+public class playingField {
+    private int size;                   // contains the length of the sides of the playing field
+    private ArrayList<Byte> tiles;      // contains all the tiles (squares) on the playing field, the content is represented by a number
+}
