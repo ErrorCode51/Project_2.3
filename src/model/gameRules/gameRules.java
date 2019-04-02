1653effe91457f4ed5ba6ef4f6877e83a9cad686
ErrorCode51 <43345236+errorcode51@users.noreply.github.com>
2019-04-02 10:53:00 +0200

    extended the gameRules interface

--- src/model/gameRules/gameRules.java
+++ src/model/gameRules/gameRules.java
@@ -7,5 +7,5 @@ public interface gameRules {
     public int getValidMoves(playingField field); // returns a list of all the moves to which a piece may be placed
 //    TODO: een return type kiezen
 
-//    TODO: meer functies toevoegen
+    public byte getGameStatus(playingField field); // returns the status of the game (you won/lost / game is still going)
 }
