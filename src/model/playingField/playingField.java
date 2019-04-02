5bc9ef6817303a334a7f14d6b87f2262205131b3
ErrorCode51 <43345236+errorcode51@users.noreply.github.com>
2019-04-02 13:00:20 +0200

    added methodes to the playing field

--- src/model/playingField/playingField.java
+++ src/model/playingField/playingField.java
@@ -1,10 +1,32 @@
 package model.playingField;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 // this class is a playing field for one of the board games
 // this class contains an arraylist of tiles
 public class playingField {
     private int size;                   // contains the length of the sides of the playing field
-    private ArrayList<Byte> tiles;      // contains all the tiles (squares) on the playing field, the content is represented by a number
+    private byte tiles[];      // contains all the tiles (squares) on the playing field, the content is represented by a number
+
+    public playingField(int size) {
+        this.size = size;
+        tiles = new byte[this.size * this.size];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public byte[][] to2dArray() {
+        if (this.tiles != null) {
+            byte[][] arrayke = new byte[this.size][this.size];
+            for (byte i = 0; i < this.size; i++) {
+                arrayke[i] = Arrays.copyOfRange(this.tiles, (i * 8), ((i * 8) + 8));
+            }
+            return arrayke;
+        }
+        else {
+            return null;
+        }
+    }
 }
