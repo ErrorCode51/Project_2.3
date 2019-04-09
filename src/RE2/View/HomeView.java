772c617191ee25aa58fdbc7d2fb45029ed4b5617
ThijsMartena <t.j.martena@st.hanze.nl>
2019-04-09 09:56:02 +0200

    added homescreen very basic

--- src/RE2/View/HomeView.java
+++ src/RE2/View/HomeView.java
@@ -3,6 +3,8 @@ package RE2.View;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,10 +18,18 @@ public class HomeView extends Application{
         VBox vBox = new VBox(0);
         Scene scene = new Scene(vBox, 350,150);
 
+        Label name = new Label("Name: ");
+
+        TextField nameInput = new TextField();
+
         Button openTTT = new Button("Open Tictactoe");
         openTTT.setOnAction(event -> new TicTacToeView());
 
-        vBox.getChildren().addAll(openTTT);
+        Button ai = new Button("AI");
+
+        Button human = new Button("Human");
+
+        vBox.getChildren().addAll(name, nameInput, openTTT, ai, human);
 
         primaryStage.setScene(scene);
         primaryStage.show();
