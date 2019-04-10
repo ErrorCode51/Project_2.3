5339fe34b822cd1e1b2a5381dd36073a367acb55
Neireau <k.t.c.gravenhorst@me.com>
2019-04-10 14:32:56 +0200

    Merged all code so far.

--- src/View/Homescreen.java
+++ src/View/Homescreen.java
@@ -8,14 +8,14 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Homescreen extends Application{
+public class Homescreen extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Home");
 
         VBox vBox = new VBox(0);
-        Scene scene = new Scene(vBox, 500,500);
+        Scene scene = new Scene(vBox, 500, 500);
 
         Label name = new Label("Name: ");
 
@@ -32,14 +32,8 @@ public class Homescreen extends Application{
         Button human = new Button("Human");
 
         vBox.getChildren().addAll(name, nameInput, openTTT, othello, ai, human);
-        Button ai = new Button("AI");
 
-        Button othello = new Button("Open othello");
         othello.setOnAction(event -> new OthelloView());
-
-        Button human = new Button("Human");
-
-        vBox.getChildren().addAll(name, nameInput, othello, openTTT, ai, human);
 
         primaryStage.setScene(scene);
         primaryStage.show();
